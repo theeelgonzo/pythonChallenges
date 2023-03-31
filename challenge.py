e5db79a1cac480fa4c3ba3a9abb7b661b82efaf1
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-30 21:06:14 -0500

    Solve second challenge

--- challenge.py
+++ challenge.py
@@ -1,6 +1,6 @@
 # given two integer numbers, return their product if the product is less than or
 # equal to 1000, otherwise return their sum
-
+"""
 def prodOrSum(num1, num2):
     product = num1 * num2
     total = num1 + num2
@@ -11,3 +11,19 @@ def prodOrSum(num1, num2):
 
 prodOrSum(20, 30)
 prodOrSum(40, 30)
+"""
+
+#write a program to iterate the first 10 numbers and in each iteration print the
+# sum of the previous and the current numbers
+
+#Printing current and previous number sum in a range(10)
+#Current Number # Previous Number # Sum: #
+
+curNum = 0
+prevNum = 0
+print('Printing current and previous number sum in a range(10)')
+for i in range(10):
+    total = curNum + prevNum
+    print(f'Current number {curNum} Previous number {prevNum} Sum: {total}')
+    prevNum = curNum
+    curNum += 1
