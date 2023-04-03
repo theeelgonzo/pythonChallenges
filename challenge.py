5115e0e7f2db08ef9992b6482e2c80b0bab6edbd
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-02 22:03:15 -0500

    Solved list challenge.

--- challenge.py
+++ challenge.py
@@ -127,3 +127,14 @@ def checkPal(number):
 checkPal(num1)
 checkPal(num2)
 
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60,  75, 90]
+list3 = []
+# odd from first, even from second
+for num in list1:
+    if num % 2 != 0:
+        list3.append(num)
+for num in list2:
+    if num % 2 == 0:
+        list3.append(num)
+print(list3)
